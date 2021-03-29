ed3f9f2b72923d9b9bfae85c35d4702cdb920ac4
AlirezaBayat <alireza@lixi.com.au>
2021-03-29 14:28:43 +1100

    Fix file name typo

--- src/components/customise-attribute/string-to-list/stringToListSchema.ts
+++
@@ -1,6 +0,0 @@
-import * as Yup from "yup";
-
-export const stringToEnumeratedListSchema = Yup.object().shape({
-  definition: Yup.string().required().min(3),
-  name: Yup.string().required().min(3),
-});
