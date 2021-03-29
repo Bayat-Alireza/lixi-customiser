84cbc22eb1f202d99c411bc3403b382b83982517
AlirezaBayat <alireza@lixi.com.au>
2021-03-29 14:36:58 +1100

    Simplify the upload component and make it more responsive on smaller screens

--- src/components/upload-file/UploadFile.tsx
+++ src/components/upload-file/UploadFile.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
 import { useStyles } from "./uploadFileStyle";
 import { Button, IconButton, Tooltip, Typography } from "@material-ui/core";
-import DescriptionTwoToneIcon from "@material-ui/icons/DescriptionTwoTone";
-import MemoryTwoToneIcon from "@material-ui/icons/MemoryTwoTone";
+// import DescriptionTwoToneIcon from "@material-ui/icons/DescriptionTwoTone";
+// import ArrowUpwardTwoToneIcon from '@material-ui/icons/ArrowUpwardTwoTone';
+// import MemoryTwoToneIcon from "@material-ui/icons/MemoryTwoTone";
+import PublishTwoToneIcon from '@material-ui/icons/PublishTwoTone';
+import FindInPageTwoToneIcon from '@material-ui/icons/FindInPageTwoTone';
 import Grid from "@material-ui/core/Grid";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableCell from "@material-ui/core/TableCell";
+import Table from "@material-ui/core/Table";
+import TableRow from "@material-ui/core/TableRow";
+import TableBody from "@material-ui/core/TableBody";
+import Paper from "@material-ui/core/Paper";
 
 interface FormProps {
   description: string;
@@ -12,8 +22,7 @@ interface FormProps {
 
 export const UploadFile: React.FC<FormProps> = ({ saveFace, description }) => {
   const classes = useStyles();
-  const [selectedFile, setSelectedFile] = React.useState<any>(undefined);
-  const [metadata, setMetadata] = React.useState<{ [key: string]: string }>();
+  const [selectedFile, setSelectedFile] = React.useState<File>();
   const handleCapture = ({ target }: any) => {
     setSelectedFile(target.files[0]);
   };
@@ -22,20 +31,8 @@ export const UploadFile: React.FC<FormProps> = ({ saveFace, description }) => {
     saveFace(selectedFile);
   };
 
-  React.useEffect(() => {
-    if (selectedFile) {
-      const meta: { [key: string]: string } = {};
-      if (!selectedFile) return;
-      for (const att in selectedFile) {
-        meta[att] = selectedFile[att];
-      }
-
-      setMetadata(meta);
-    }
-  }, [selectedFile]);
-
   return (
-    <>
+    <div style={{width:"100%"}}>
       <input
         accept=".xml,.xsd"
         className={classes.input}
@@ -43,49 +40,46 @@ export const UploadFile: React.FC<FormProps> = ({ saveFace, description }) => {
         type="file"
         onChange={handleCapture}
       />
-      <Grid container spacing={1} alignItems="center">
-        <Grid item>
-          <Tooltip title="Click to select LIXI Base Schema">
+      <Grid wrap="nowrap" alignContent="flex-start" container spacing={1} alignItems="center">
+        <Grid item >
+          <Tooltip title="">
             <label
-              style={{ display: "flex", alignItems: "center" }}
+              className={classes.label}
               htmlFor="document"
             >
               <IconButton
-                className={classes.documentImage}
+                className={classes.findPageIcon}
                 color="primary"
                 aria-label="upload picture"
                 component="span"
+                title="Browse"
               >
-                <DescriptionTwoToneIcon fontSize="large" />
+                <FindInPageTwoToneIcon fontSize="large" />
               </IconButton>
               <Typography
-                style={{
-                  backgroundColor: "#c5cae9",
-                  padding: "0.3rem",
-                  borderRadius: "0.2rem",
-                }}
-                variant="caption"
+                variant="body2"
                 className={classes.fileName}
                 noWrap
               >
-                {metadata ? metadata.name : description}
+                Choose the file
               </Typography>
             </label>
           </Tooltip>
         </Grid>
-        <Grid item>
-          <Button
-            style={{ marginLeft: "0.5rem",    minWidth:"6rem" }}
-            startIcon={<MemoryTwoToneIcon />}
-            variant="outlined"
-            size="small"
-            onClick={() => handleSubmit()}
-            color="primary"
+        <Grid item  >
+        {selectedFile?.name?<IconButton
+          className={classes.loadIcon}
+          onClick={() => handleSubmit()}
+          color="primary"
+          aria-label="upload file"
+          component="span"
+          title="Load"
           >
-            Upload
-          </Button>
+            <PublishTwoToneIcon fontSize="large" />
+          </IconButton>:undefined}
         </Grid>
       </Grid>
-    </>
+       
+    </div>
   );
 };
