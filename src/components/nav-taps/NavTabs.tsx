71dd5bf419c072b9833deb16fc554d1fa50dcad3
AlirezaBayat <alireza@lixi.com.au>
2021-04-14 12:05:58 +1000

    Comment unused code

--- src/components/nav-taps/NavTabs.tsx
+++ src/components/nav-taps/NavTabs.tsx
@@ -8,10 +8,10 @@ import Typography from "@material-ui/core/Typography";
 import { VerticalLinearStepper } from "../vertical-stepper/VerticalStepper";
 import { ItemXMLViewer } from "../itemXMLViewer/ItemXMLViewer";
 import { useTypedSelector } from "../../hooks/useTypeSelector";
-import { SearchPath } from "../search/SearchPath";
-import { LixiItem } from "../lixi-item/LixiItem";
-import { Customiser } from "../../models/Customiser";
-import { LixiBase } from "../../models/LixiBase";
+// import { SearchPath } from "../search/SearchPath";
+// import { LixiItem } from "../lixi-item/LixiItem";
+// import { Customiser } from "../../models/Customiser";
+// import { LixiBase } from "../../models/LixiBase";
 import { CreateInstructionTab } from "./create-instruction/CreateInstruction";
 import Toolbar from "@material-ui/core/Toolbar";
 import { TabPanel } from "./NavTabPanel";
@@ -29,12 +29,12 @@ export const NavTabs: React.FC = () => {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState(0);
-  const [itemInstruction, setItemInstruction] = React.useState<Element>();
-  const { customization, customisedItem, subSchema } = useTypedSelector(
+  // const [itemInstruction, setItemInstruction] = React.useState<Element>();
+  const { customization, subSchema } = useTypedSelector(
     (state) => state.customizer
   );
   const { schema } = useTypedSelector((state) => state.schema);
-  const { data } = useTypedSelector((state) => state.item);
+  // const { data } = useTypedSelector((state) => state.item);
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
   };
@@ -48,18 +48,18 @@ export const NavTabs: React.FC = () => {
     }
   }, [schema, subSchema?.transactionType, open]);
 
-  React.useEffect(() => {
-    console.log("data:", data);
-    if (!data) return;
-    const path = new LixiBase(data)?.path;
-    if (path) {
-      const newCustomiser = new Customiser(customization, path);
-      const customisedItem = newCustomiser?.getCustomisedItem()?.parentElement;
-      console.log("customisedItem", customisedItem);
-      if (!customisedItem) return;
-      setItemInstruction(customisedItem);
-    }
-  }, [customization, data]);
+  // React.useEffect(() => {
+  //   console.log("data:", data);
+  //   if (!data) return;
+  //   const path = new LixiBase(data)?.path;
+  //   if (path) {
+  //     const newCustomiser = new Customiser(customization, path);
+  //     const customisedItem = newCustomiser?.getCustomisedItem()?.parentElement;
+  //     console.log("customisedItem", customisedItem);
+  //     if (!customisedItem) return;
+  //     setItemInstruction(customisedItem);
+  //   }
+  // }, [customization, data]);
 
   return (
     <div className={classes.root}>
